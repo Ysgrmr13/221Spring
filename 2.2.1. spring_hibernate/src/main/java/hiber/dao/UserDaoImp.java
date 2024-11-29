bc59ccef24f3653b86f9e763f6277dbaeed6fc0e
Ysgrmr13 <Ysgrmr13@gmail.com>
2024-11-29 16:58:12 +0300

    lada

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,11 +1,17 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.context.spi.CurrentSessionContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
+import java.util.Currency;
 import java.util.List;
 
 @Repository
@@ -22,8 +28,25 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
 
+   @Override
+   @SuppressWarnings("unchecked")
+   public void deleteAllUsers() {
+      List<User> users = listUsers();
+      for (User user: users) {
+         sessionFactory.getCurrentSession().delete(user);
+      }
+   }
+
+   @Override
+   public User findOwner(String car_name, String car_series) { // Измените тип параметра car_series на String
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(
+              "select u from User u join u.car c where c.model = :model and c.series = :series", User.class);
+      query.setParameter("model", car_name);
+      query.setParameter("series", car_series); // Измените тип параметра car_series на String
+      return query.getSingleResult();
+   }
 }
